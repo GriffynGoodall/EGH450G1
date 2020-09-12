e19aae260c89cac9d8ba3520f4c59520c59ab66e
Griffyn <griffyn.goodall@connect.qut.edu.au>
2020-09-12 22:19:22 +1000

    First Update

---
+++ build/egh450g1_code/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/griff/catkin_ws/src/egh450g1_code
+# Build directory: /home/griff/catkin_ws/build/egh450g1_code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/egh450g1_code/catkin_generated/setup_py_interrogation.cmake
@@ -0,0 +1,7 @@
+set(egh450g1_code_SETUP_PY_SETUP_MODULE "distutils.core")
+set(egh450g1_code_SETUP_PY_VERSION "0.0.0")
+set(egh450g1_code_SETUP_PY_SCRIPTS "")
+set(egh450g1_code_SETUP_PY_PACKAGES "egh450g1_code")
+set(egh450g1_code_SETUP_PY_PACKAGE_DIRS "src/egh450g1_code")
+set(egh450g1_code_SETUP_PY_MODULES "")
+set(egh450g1_code_SETUP_PY_MODULE_DIRS "")
